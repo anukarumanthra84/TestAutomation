9b9e6559e6228015e54e086870b4c481786d7627
anukarumanthra84 <anu.oct20@gmail.com>
2021-12-25 08:54:11 +0000

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my first commit integrating with jenkins");
+        System.out.println("This is commit from feature to master");
+        System.out.println("This demo for no conflict");
+        System.out.println("This demo");
+    }
+    void hello(){
+        
+    }
+}
